a98ca5c175d29042dc533aa0370e6718f96f135d
Stanisław Horna <stanislawhorna@outlook.com>
2025-07-10 22:21:27 +0200

    update to use http backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,3 +2,14 @@ provider "vault" {
   token   = var.vault_token
   address = var.vault_url
 }
+terraform {
+  backend "http" {
+    address = "http://10.0.10.11:8001/state/HashiCorp_Vault/prod"
+  }
+}
+data "terraform_remote_state" "foo" {
+  backend = "http"
+  config = {
+    address = "http://10.0.10.11:8001/state/HashiCorp_Vault/prod"
+  }
+}
